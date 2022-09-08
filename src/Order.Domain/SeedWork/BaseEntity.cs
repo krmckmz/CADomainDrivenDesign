cab154b017325268b832374ac2a7483076c96da6
krmckmz <keremcokmez@gmail.com>
2022-09-08 03:35:30 +0300

    added application

--- src/Order.Domain/SeedWork/BaseEntity.cs
+++ src/Order.Domain/SeedWork/BaseEntity.cs
@@ -1,5 +1,17 @@
 namespace Order.Domain;
+using System.Collections;
+using System.Collections.Generic;
+
 public abstract class BaseEntity
 {
     public int Id { get; set; }
+    private ICollection<INotification> domainEvents;
+     public ICollection<INotification> DomainEvents=>domainEvents;
+
+     public void AddDomainEvents(INotification notification)
+     {
+        domainEvents ??= new List<INotification>();
+
+        domainEvents.Add(notification);
+     }
 }
